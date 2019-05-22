ec764a28a1f13101bd5969f092632cccbc3711e7
Fawaz Siddiqi <NA>
2019-05-23 02:55:39 +0400

    Initial Commit

---
+++ ServerClientIntegrity/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ServerClientIntegrity
+//
+//  Created by Fawaz Siddiqi on 5/23/19.
+//  Copyright © 2019 Fawaz Siddiqi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
